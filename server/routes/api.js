3635b06b9f500ebf7827fa537c67ab42f306e19b
Brandon Davis <bdavis171@gmail.com>
2020-06-18 08:37:33 -0500

    completed necessary routes for GameSchema

--- server/routes/api.js
+++ server/routes/api.js
@@ -12,6 +12,7 @@ const secretKey = require('../config/keys').secretOrKey;
 
 // import schemas
 const GameCollection = require('../models/GameSchema');
+const UserCollection = require('../models/UserSchema');
 
 
 ///////////////////////////////////////////////////////////////
@@ -22,16 +23,75 @@ const GameCollection = require('../models/GameSchema');
 
 // POST: add a new game
 router.post("/games",(req,res)=> {
-    res.send("new game added");
+    // res.send("new game added");
+    GameCollection.create(req.body,(errors,results) => {
+        errors ? res.send(errors):res.send(results);
+    });
 });
 
 // GET: view all games
 router.get("/games",(req,res) => {
-    res.send("all games viewed")
+    // res.send("all games viewed")
+    GameCollection.find((errors,results) => {
+        errors ? res.send(errors):res.send(results);
+    });
 });
 
 // GET: view one game by id
-router.get("")
+router.get("/games/:id",(req,res) => {
+    // res.send("one game viewed");
+    GameCollection.findById(req.params.id,(errors,results) => {
+        errors ? res.send(errors):res.send(results);
+    });
+});
+
+// GET: view game(s) by title
+router.get("/games/searchByTitle/:title",(req,res) => {
+    // res.send("game(s) found by title");
+    GameCollection.find({title: req.params.title},(errors,results) => {
+        errors ? res.send(errors):res.send(results);
+    });
+
+});
+
+// PUT: edit one game by id
+router.put("/games/:id",(req,res) => {
+    // res.send("one game updated");
+    GameCollection.findByIdAndUpdate(req.params.id,req.body,{new: true},(errors,results) => {
+        errors ? res.send(errors):res.send(results);
+    });
+});
+
+// DELETE: delete one game by id
+router.delete("/games/:id",(req,res) => {
+    // res.send("one game deleted");
+    GameCollection.findByIdAndDelete(req.params.id,(errors,results) => {
+        errors ? res.send(errors):res.send(results);
+    });
+});
+
+// PUT: relate a game to a user
+router.put("/games/relate/:gameID/:email",async (req,res) => {
+    // res.send('game has been related');
+    let game,user;
+    await GameCollection.findById(req.params.gameID,(errors,results) => {
+        if(errors){
+            res.send(errors);
+        } else {
+            game = results;
+            UserCollection.findOne({email: req.params.email},(errors,results) => {
+                if(errors){
+                    res.send(errors);
+                } else {
+                    user = results;
+                    user.gamesOwned.push(game._id);
+                    user.save();
+                    res.send(user);
+                }
+            })
+        }
+    })
+})
 
 // export routes
 module.exports = router;
